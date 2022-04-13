53e51e6d7316b8ad059a1fd4baf55aae0f8c5cde
Сергей Кастов <smailix1@gmail.com>
2022-04-13 14:48:25 +0300

    2

--- yatube/posts/test/test_views.py
+++ yatube/posts/test/test_views.py
@@ -2,7 +2,6 @@ from http import HTTPStatus
 
 import shutil
 import tempfile
-from urllib import response
 
 from django.contrib.auth import get_user_model
 from django.test import TestCase, Client, override_settings
